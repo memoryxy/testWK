33f77a7fba322ab2f62deaf5cd6ce3567932980e
wangjianfei <hzwangjianfei@corp.netease.com>
2018-04-03 14:33:09 +0800

    sss

--- testWKUITests/testWKUITests.m
+++ testWKUITests/testWKUITests.m
@@ -15,7 +15,7 @@
 @implementation testWKUITests
 
 - (void)setUp {
-    [super setUp];
+    [super setUp]; 
     
     // Put setup code here. This method is called before the invocation of each test method in the class.
     
